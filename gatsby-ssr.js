710c8de874df1385a1d594736e2705eec21ab2b4
jon <jcoleman1388@gmail.com>
2020-10-27 07:29:32 -0400

    working on FOUC issue

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,6 +1,15 @@
 import React from "react";
 import Layout from "./src/components/Layout";
+import "gatsby-prismjs-dracula";
+import GlobalStyles from "./src/styles/GlobalStyles";
+import Typography from "./src/styles/Typography";
 
 export function wrapPageElement({ element, props }) {
-  return <Layout {...props}>{element}</Layout>;
+  return (
+    <>
+      <GlobalStyles />
+      <Typography />
+      <Layout {...props}>{element}</Layout>
+    </>
+  );
 }
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,5 @@
 import React from "react";
 import "normalize.css";
-import GlobalStyles from "../styles/GlobalStyles";
-import Typography from "../styles/Typography";
 import Nav from "./Nav";
 import Menu from "./Menu";
 import ExternalLinks from "./Links";
@@ -9,8 +7,6 @@ import ExternalLinks from "./Links";
 export default function Layout({ children }) {
   return (
     <>
-      <GlobalStyles />
-      <Typography />
       <Nav />
       <Menu />
       <ExternalLinks />
