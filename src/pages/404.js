7163b107db73762ad964b9d58ad14be594d8c167
jon <jcoleman1388@gmail.com>
2020-11-01 16:29:23 -0500

    added 404

--- src/pages/404.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function FourZeroFour() {
-  return (
-    <>
-      <p>That page is not here.</p>
-    </>
-  );
-}
---
+++ src/pages/default404.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "gatsby";
+
+export default function FourZeroFour() {
+  return (
+    <>
+      <h2>Looks like that page is not here.</h2>
+      <Link to="/">Home</Link>
+    </>
+  );
+}
